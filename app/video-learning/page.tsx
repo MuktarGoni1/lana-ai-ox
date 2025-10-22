adc973fab1592e3b461fcc689c5089c32d4f55b9
MuktarGoni1 <muktargoni1@email.com>
2025-10-22 21:49:06 +0100

    2nd update(incomplete landing)

--- app/video-learning/page.tsx
+++
@@ -1,5 +0,0 @@
-import VideoLearningPage from "@/components/video-learning-page"
-
-export default function VideoLearning() {
-  return <VideoLearningPage />
-}
--- lib/db.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import { env } from './env'
-
-const url = env.SUPABASE_URL!
-const key = env.SUPABASE_ANON_KEY!
-
-export const supabase = createClient(url, key)
